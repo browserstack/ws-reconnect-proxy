7418438f0c4fe2a2038ad87f0ee071abcc942860
Yash Ladha <yashladhapankajladha123@gmail.com>
2021-07-09 07:53:01 +0530

    chore: remove redundant information from child logger
    Removed host since it is already being covered in hostname key inserted
    by the pino logger.

--- loggerFactory.js
+++ loggerFactory.js
@@ -1,13 +1,11 @@
 'use strict';
 
-const os = require('os');
 const Logger = require('pino')();
 
 Logger.level = 'debug';
 
 function getDefaultObjects() {
   return {
-    host: os.hostname(),
     pid: process.pid
   };
 }
